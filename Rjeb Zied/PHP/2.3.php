6e12bfde129db8425c9d4b3cf65ccc0d18fce7af
Rzied <59011-95-11@exchange.ad.afpanet>
2020-09-15 09:19:20 +0200

    gitignore

--- Rjeb Zied/PHP/2.3.php
+++ Rjeb Zied/PHP/2.3.php
@@ -3,9 +3,9 @@
     $HT=readline ("entrer le prix HT :");
     $NB=readline ("entrer le nembre d'article :");
     $TAUX=readline ("entrer le taux de TVA :");
-    $cons=$ht/100
+    $TTC=0;
 
-    $T=($cons*$TAUX);
+    $T=($HT/100)*$TAUX;
     $TTC=($T+$TTC)*$NB;
 
     echo $TTC;
