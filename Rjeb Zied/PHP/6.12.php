bd26ea69ff66bce54eef4df05a40e850cb2104c0
Rzied <59011-95-11@exchange.ad.afpanet>
2020-09-16 17:25:25 +0200

    6.12

---
+++ Rjeb Zied/PHP/6.12.php
@@ -0,0 +1,13 @@
+<?php
+
+do {
+    do {
+        $n = readline("Entrer  le nombre de valeur que vous comptez saisir :");
+    } while (!is_numeric($n));
+} while (!is_int($n * 1));
+
+for ($i = 1; $i ==$n ; $i++) {
+    do {
+        $tab[$i] = readline("Entrer la note " . $i . " :(ça dois etre un nombre!)\t");
+    } while (!is_numeric($tab[$i]));
+}
