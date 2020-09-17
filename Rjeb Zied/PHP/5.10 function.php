0ad82a620607741b56732a7c74d61c679cd4180a
Rzied <59011-95-11@exchange.ad.afpanet>
2020-09-17 16:38:20 +0200

    7.2

--- Rjeb Zied/PHP/5.10 function.php
+++ Rjeb Zied/PHP/5.10 function.php
@@ -1,11 +1,10 @@
 <?php
 // Fonction de saisi de nombre
 function demandeEntierPhrase($msg) // Demande un entier à l'utilisateur
+
 {
-    do
-    {
-        do
-        {
+    do {
+        do {
             $nombre = readline($msg);
         } while (!is_numeric($nombre)); // on verifie que la chaine de caracterer ne contient que des chiffres
     } while (!is_int($nombre * 1)); // on vérifie que le nombre est entier (pas réel)
@@ -13,25 +12,23 @@ function demandeEntierPhrase($msg) // Demande un entier à l'utilisateur
 }
 
 function factorielle($nombre) //calcule la factorielle d'un nombre
+
 {
     $facto = 1;
 
 /* Calculer la factorielle */
-    for ($i = 2; $i <= $nombre; $i++)
-    {
+    for ($i = 2; $i <= $nombre; $i++) {
         $facto = $facto * $i; //$facto *=$i;
     }
     return $facto;
 }
 
-
-$n=demandeEntierPhrase("Entrer le nombre de chevaux partants : ");
-$p=demandeEntierPhrase("le nombre de chevaux joués: ");
-
+$n = demandeEntierPhrase("Entrer le nombre de chevaux partants : ");
+$p = demandeEntierPhrase("le nombre de chevaux joués: ");
 
 $factN = factorielle($n);
 $factP = factorielle($p);
-$factNp = factorielle($n-$p);
+$factNp = factorielle($n - $p);
 
 $x = $factN / $factNp;
 $y = $factN / ($factP * $factNp);
---
+++ Rjeb Zied/PHP/7.2.php
@@ -0,0 +1,48 @@
+<?php
+
+// Fonction de saisi de la taille de tableau
+function saisi()
+{
+    do {
+        do {
+            $n = readline("Entrer la taille du tableau :");
+        } while (!is_numeric($n));
+    } while (!is_int($n * 1));
+    return $n;
+}
+
+// remplire le tableau
+function remplir($n)
+{
+
+    for ($i = 0; $i < $n; $i++) {
+        do {
+            $tab[$i] = readline("Entrer la valeur N° " . ($i + 1) . " :");
+        } while (!is_numeric($tab[$i]));
+    }
+    return $tab;
+}
+
+function affichage($tab) {
+    for ($i = 0;$i<count($tab);$i++) {
+        echo $tab[$i]."\t";
+    } 
+}
+
+
+$n = saisi();
+$tab = remplir($n);
+$test=true;
+while ($test) {
+    $test=false;
+        for ($i=0; $i <$n-1 ; $i++) { 
+           if ($tab[$i]<$tab[$i+1]) {
+               $temp=$tab[$i];
+               $tab[$i]=$tab[$i+1];
+               $tab[$i+1]=$temp;
+               $test=true;
+           }
+        }
+}
+echo "\n Le tableau trié dans l’ordre décroissant : \n";
+affichage($tab);
