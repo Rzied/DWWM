5b7fac53793c8efbc05998ecde7a1620bba529f0
Rzied <59011-95-11@exchange.ad.afpanet>
2020-09-18 10:54:13 +0200

    6.13 fini

--- Rjeb Zied/PHP/6.13.php
+++ Rjeb Zied/PHP/6.13.php
@@ -13,13 +13,15 @@ for ($i = 1; $i <= $n; $i++) {
 }
 // chercher la plus grande valeur du tab
 // on peut utiliser la fonction max 
-$pg=$tab[1];
-for ($i=2; $i <=$n ; $i++) { 
-    if ($tab[$i]>$pg) {
-        $pg=$tab[$i];
-        $p=$i;
-    }
-}
-echo "la plus grande valeur est : ",$pg,"\n";
-echo "la position qu'elle occupe est : ",$p;
-// echo "la valeur la plus haute est :".max($tab).", elle se trouve à la position : ".array_search(max($tab),$tab);
+// $pg=$tab[1];
+// for ($i=2; $i <=$n ; $i++) { 
+//     if ($tab[$i]>$pg) {
+//         $pg=$tab[$i];
+//         $p=$i;
+//     }
+// }
+// echo "la plus grande valeur est : ",$pg,"\n";
+// echo "la position qu'elle occupe est : ",$p;
+
+
+echo "la valeur la plus haute est :".max($tab).", elle se trouve à la position : ".array_search(max($tab),$tab);
