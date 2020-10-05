fb01ad9571e5b4ee1d7b109ef01e8068673310ab
59011-95-11 <59011-95-11@exchange.ad.afpanet>
2020-10-05 17:27:13 +0200

    dates

--- Rjeb Zied/PHP/POO/Employe.class.php
+++ Rjeb Zied/PHP/POO/Employe.class.php
@@ -1,20 +1,150 @@
 <?php
+function ChargerClasse($classe)
+{
+    require $classe . ".Class.php";
+}
+spl_autoload_register("ChargerClasse");
 
 class Employe
 {
 
-    //Attributs
+    /*****************Attributs***************** */
     private $_nom;
     private $_prenom;
     private $_dated;
     private $_poste;
     private $_salaire;
     private $_service;
-    
 
+    /*****************Accesseurs***************** */
+    public function getNom()
+    {
+        return $this->_nom;
+    }
 
+    public function setNom($nom)
+    {
+        $this->_nom = $nom;
+    }
 
+    public function getPrenom()
+    {
+        return $this->_prenom;
+    }
 
+    public function setPrenom($prenom)
+    {
+        $this->_prenom = $prenom;
+    }
+
+    public function getDated()
+    {
+        return $this->_dated;
+    }
+
+    public function setDated($dated)
+    {
+        $this->_dated = $dated;
+    }
+
+    public function getPoste()
+    {
+        return $this->_poste;
+    }
+
+    public function setPoste($poste)
+    {
+        $this->_poste = $poste;
+    }
+
+    public function getSalaire()
+    {
+        return $this->_salaire;
+    }
+
+    public function setSalaire($salaire)
+    {
+        $this->_salaire = $salaire;
+    }
+
+    public function getService()
+    {
+        return $this->_service;
+    }
+
+    public function setService($service)
+    {
+        $this->_service = $service;
+    }
+
+    /*****************Constructeur***************** */
+
+    public function __construct(array $options = [])
+    {
+        if (!empty($options)) // empty : renvoi vrai si le tableau est vide
+        {
+            $this->hydrate($options);
+        }
+    }
+    public function hydrate($data)
+    {
+        foreach ($data as $key => $value) {
+            $methode = "set" . ucfirst($key); //ucfirst met la 1ere lettre en majuscule
+            if (is_callable(([$this, $methode]))) // is_callable verifie que la methode existe
+            {
+                $this->$methode($value);
+            }
+        }
+    }
+
+    /*****************Autres Méthodes***************** */
+
+    /**
+     * Transforme l'objet en chaine de caractères
+     *
+     * @return String
+     */
+    public function toString()
+    {
+        return "";
+    }
+
+    /**
+     * Renvoi vrai si l'objet en paramètre est égal à l'objet appelant
+     *
+     * @param [type] $obj
+     * @return bool
+     */
+    public function equalsTo($obj)
+    {
+        return true;
+    }
+    /**
+     * Compare 2 objets
+     * Renvoi 1 si le 1er est >
+     *        0 si ils sont égaux
+     *        -1 si le 1er est <
+     *
+     * @param [type] $obj1
+     * @param [type] $obj2
+     * @return void
+     */
+    public static function compareTo($obj1, $obj2)
+    {
+        return 0;
+    }
+
+    public function Anciennete()
+    {
+        return date("y") - $this->getDated();
+    }
+
+    public function prime()
+    {
+
+        $prime=((($this->getSalaire()*1000)/100)*5)+((($this->getSalaire()*1000)/100)*2);
+        return $prime;
+    }
 
 
 }
--- Rjeb Zied/PHP/POO/te.php
+++ Rjeb Zied/PHP/POO/te.php
@@ -1,6 +1,17 @@
 <?php
-function ChargerClasse($classe)
-{
-    require $classe.".Class.php";
-}
-spl_autoload_register("ChargerClasse");
+// function ChargerClasse($classe)
+// {
+//     require $classe.".Class.php";
+// }
+// spl_autoload_register("ChargerClasse");
+
+// echo date("d.m.y");
+
+$date = new DateTime("2012-09-01");
+// $date->modify("+ 2 days");
+echo $date->format('d-m-y');
+echo "\n";
+$date1=new DateTime("now");
+
+$diff=$date1->diff($date);
+echo $diff->format('%d Jours %m Mois %y Ans ');
