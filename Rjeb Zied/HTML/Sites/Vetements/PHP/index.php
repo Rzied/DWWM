539b9f4dea021f92634e37ee7801739cfd9d9e2b
Rzied <galaxyzied@gmail.com>
2020-11-12 20:43:42 +0100

    chauss fini

---
+++ Rjeb Zied/HTML/Sites/Vetements/PHP/index.php
@@ -0,0 +1,7 @@
+<?php
+$titreonglet="Accueil";
+include "./view/head.php";
+include "./view/header.php";
+include "./view/nav.php";
+include "./view/session.php";
+include "./view/footer.php";
---
+++ Rjeb Zied/PHPWeb/VirtualhostZied/Vetements/PHP/VIEW/head.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $titreonglet;?></title>
+	<link rel="stylesheet" href="../CSS/style.css">
+	<script src="../JS/script.js"></script>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="page colonne">
+		<div class="tete">
