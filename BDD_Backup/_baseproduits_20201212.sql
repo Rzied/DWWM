a5e7bbd0be12a8f85d2815621f767243a79b1768
59011-95-11 <59011-95-11@exchange.ad.afpanet>
2020-12-12 12:15:36 +0100

    12/12/2020 12:15:35,21

--- BDD_Backup/_baseproduits_20201212.sql
+++ BDD_Backup/_baseproduits_20201212.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.31, for Win64 (x86_64)
 --
--- Host: localhost    Database: baseproduitsmoi
+-- Host: localhost    Database: baseproduits
 -- ------------------------------------------------------
 -- Server version	5.7.31
 
@@ -16,37 +16,35 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `baseproduitsmoi`
+-- Current Database: `baseproduits`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `baseproduitsmoi` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `baseproduits` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `baseproduitsmoi`;
+USE `baseproduits`;
 
 --
--- Table structure for table `clients`
+-- Table structure for table `categories`
 --
 
-DROP TABLE IF EXISTS `clients`;
+DROP TABLE IF EXISTS `categories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `clients` (
-  `idClient` int(11) NOT NULL AUTO_INCREMENT,
-  `nomClient` varchar(50) NOT NULL,
-  `prenomClient` varchar(50) NOT NULL,
-  `dateDeNaissance` date NOT NULL,
-  PRIMARY KEY (`idClient`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+CREATE TABLE `categories` (
+  `idCategorie` int(11) NOT NULL AUTO_INCREMENT,
+  `libelleCategorie` varchar(50) NOT NULL,
+  PRIMARY KEY (`idCategorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `clients`
+-- Dumping data for table `categories`
 --
 
-LOCK TABLES `clients` WRITE;
-/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
-INSERT INTO `clients` VALUES (1,'RJEB','Zied','2002-11-30'),(2,'DUPON','Patrik','2000-10-30'),(3,'lecler','Pierre','2002-11-30'),(4,'POIX','Sofiane','2000-10-30');
-/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Rouge'),(2,'Orange'),(3,'Jaune'),(4,'Vert'),(5,'Bleu'),(6,'Rose');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -61,11 +59,11 @@ CREATE TABLE `produits` (
   `libelleProduit` varchar(50) NOT NULL,
   `prix` int(11) NOT NULL,
   `dateDePeremption` date NOT NULL,
-  `idClient` int(11) NOT NULL,
+  `idCategorie` int(11) NOT NULL,
   PRIMARY KEY (`idProduit`),
-  KEY `idClient` (`idClient`),
-  CONSTRAINT `produits_ibfk_1` FOREIGN KEY (`idClient`) REFERENCES `clients` (`idClient`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  KEY `produits_categories_FK` (`idCategorie`),
+  CONSTRAINT `produits_categories_FK` FOREIGN KEY (`idCategorie`) REFERENCES `categories` (`idCategorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,7 +72,7 @@ CREATE TABLE `produits` (
 
 LOCK TABLES `produits` WRITE;
 /*!40000 ALTER TABLE `produits` DISABLE KEYS */;
-INSERT INTO `produits` VALUES (1,'gomme',2,'2020-11-30',1),(2,'crayon',1,'2020-11-30',2),(3,'stylo',5,'2022-11-30',2),(4,'livre',13,'2020-01-03',1),(5,'feutre',20,'2020-10-11',3),(6,'marqueur',11,'2020-07-03',3);
+INSERT INTO `produits` VALUES (1,'gomme',2,'2020-11-30',1),(2,'crayon',1,'2020-11-30',2),(3,'règle',5,'2020-10-30',3),(4,'stylo',2,'2020-12-30',5),(5,'cahier',12,'2020-11-30',6),(6,'classeur',15,'2020-11-30',1),(7,'feuilles',2,'2020-11-30',5),(8,'pochette',6,'2020-11-20',3);
 /*!40000 ALTER TABLE `produits` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -87,4 +85,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-11 17:21:53
+-- Dump completed on 2020-12-12 12:10:52
