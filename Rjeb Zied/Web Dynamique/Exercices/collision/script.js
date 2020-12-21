7bcdc45b972846f19910e1c33c42d242b3bf809e
Rzied <galaxyzied@gmail.com>
2020-12-21 15:15:05 +0100

    collision

---
+++ Rjeb Zied/Web Dynamique/Exercices/collision/script.js
@@ -0,0 +1,53 @@
+var carre = document.getElementById("obj");
+var distance = 5;
+
+
+function deplace(dx, dy) {
+    var deplacement_ok = true;
+    var styleCarre = window.getComputedStyle(document.getElementById('carre'), null);
+    var t = parseInt(styleCarre.top);
+    var l = parseInt(styleCarre.left);
+    var w = parseInt(styleCarre.width);
+    var h = parseInt(styleCarre.height);
+    var listeObs = document.querySelectorAll('.obstacle');
+    listeObs.forEach(function (elt) {
+        var styleObst = window.getComputedStyle(elt, null);
+        var tob = parseInt(styleObst.top);
+        var lob = parseInt(styleObst.left);
+        var wob = parseInt(styleObst.width);
+        var hob = parseInt(styleObst.height);
+        deplacement_ok = deplacement_ok && depl_ok(tob, lob, wob, hob, t + dy, l + dx, w, h);
+
+    });
+    if (deplacement_ok) {
+        document.getElementById('carre').style.top = t + dy + 'px';
+        document.getElementById('carre').style.left = l + dx + 'px';
+    }
+}
+
+function depl_ok(tob, lob, wob, hob, t, l, w, h) {
+    if (l < lob + wob && l + w > lob && t < tob + hob && t + h > tob) {
+        return false
+    }
+    return true;
+}
+
+var carre = document.getElementById("carre");
+
+window.addEventListener("keydown", function (event) {
+
+    switch (event.key) {
+        case "ArrowDown":
+            deplace(0, 5);
+            break;
+        case "ArrowUp":
+            deplace(0, -5);
+            break;
+        case "ArrowLeft":
+            deplace(-5, 0);
+            break;
+        case "ArrowRight":
+            deplace(5, 0);
+    }
+
+}, );
