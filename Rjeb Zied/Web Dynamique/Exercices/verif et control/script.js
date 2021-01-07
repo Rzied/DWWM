e0f8202ae74c2dd8944b984a4c29805f85506da0
Rzied <galaxyzied@gmail.com>
2021-01-07 10:47:19 +0100

    form

--- Rjeb Zied/Web Dynamique/Exercices/verif et control/script.js
+++ Rjeb Zied/Web Dynamique/Exercices/verif et control/script.js
@@ -31,33 +31,33 @@ lesInputs.forEach((item) => {
             break;
         case "prenom":
             item.addEventListener("input", function (e) {
-                    veString(e.target)
-                    // for (let i = 0; i < e.target.value.length; i++) {
-                    //     if (!isLetter(e.target.value[i])) {
-                    //         alert("prenom")
+                veString(e.target)
+                // for (let i = 0; i < e.target.value.length; i++) {
+                //     if (!isLetter(e.target.value[i])) {
+                //         alert("prenom")
                 // }
                 // }
             })
-    break;
-    case "ddn":
-    ddn.addEventListener("change", veDdn)
+            break;
+        case "ddn":
+            ddn.addEventListener("change", veDdn)
 
-    break;
-    case "codePostal":
-    // codePostal.addEventListener("keyup", function (e) {
-    //     // for (let i = 0; i < e.target.value.length; i++) {
-    //         if (isNaN(e.target.value) || (e.target.value.length > 5)) {
-    //             alert("toto"+e.target.value);
-    //             console.log(e.target.value);
-    //         }
-    //     // }
-    // })
-    codePostal.addEventListener("input", veCodePostal);
-    break;
-    default:
-    break;
-}
-// });
+            break;
+        case "codePostal":
+            // codePostal.addEventListener("keyup", function (e) {
+            //     // for (let i = 0; i < e.target.value.length; i++) {
+            //         if (isNaN(e.target.value) || (e.target.value.length > 5)) {
+            //             alert("toto"+e.target.value);
+            //             console.log(e.target.value);
+            //         }
+            //     // }
+            // })
+            codePostal.addEventListener("input", veCodePostal);
+            break;
+        default:
+            break;
+    }
+    // });
 })
 
 function isLetter(c) {
@@ -66,12 +66,16 @@ function isLetter(c) {
 
 function veString(input) {
     console.log(input);
+    
+    var valide = input.parentNode.getElementsByClassName("valide")[0];
+    var croix = input.parentNode.getElementsByClassName("croix")[0];
     var msgE = input.parentNode.getElementsByClassName("msg")[0];
     var inputVal = input.value;
     console.log(inputVal);
     if (!isNaN(inputVal)) {
         input.value = input.value.slice(0, -1);
         input.style.border = "3px solid red";
+        croix.style.display="flex";
         msgE.innerHTML = "Veuillez ecrire que des lettres !";
 
         if (input.name == "nom") {
@@ -79,6 +83,8 @@ function veString(input) {
         }
     } else if (input.checkValidity()) {
         input.style.border = "3px solid green";
+        croix.style.display="none";
+        valide.style.display="flex";
         msgE.innerHTML = "";
         if (input == "nom") {
             flag1 = true;
@@ -87,12 +93,16 @@ function veString(input) {
         input.value = input.value.slice(0, -1);
         // input.style.border = "3px solid red";
         msgE.innerHTML = "Veuillez ecrire que des lettres !";
+        valide.style.display="none";
+        croix.style.display="flex";
     }
     btnEnvoyer(flag1, flag2, flag3);
 }
 
 function veDdn() {
 
+    var valide = ddn.parentNode.getElementsByClassName("valide")[0];
+    var croix = ddn.parentNode.getElementsByClassName("croix")[0];
     let dateUser = ddn.value;
     let jour = parseInt(dateUser.substring(8, 10));
     let mois = parseInt(dateUser.substring(5, 7));
@@ -103,10 +113,14 @@ function veDdn() {
     if (date > dateSysteme) {
         msgDDN.innerHTML = "la date ne peux pas etre superieur à auj !";
         ddn.style.border = "3px solid red";
+        croix.style.display="flex";
+        valide.style.display="none";
         flag2 = false;
     } else {
         msgDDN.innerHTML = "";
         ddn.style.border = "3px solid green";
+        croix.style.display="none";
+        valide.style.display="flex";
         flag2 = true;
     }
     btnEnvoyer(flag1, flag2, flag3);
@@ -116,20 +130,26 @@ function veCodePostal() {
     var valeur = codePostal.value.toLowerCase();
     var touche = valeur.charCodeAt(valeur.length - 1);
     var msgCP = codePostal.parentNode.getElementsByClassName("msg")[0];
+    var valide = codePostal.parentNode.getElementsByClassName("valide")[0];
+    var croix = codePostal.parentNode.getElementsByClassName("croix")[0];
     // var checkInputCP = (codePostal.parentNode).getElementsByClassName("checkInput")[0];
 
     if (touche >= 97 && touche <= 122) {
         codePostal.value = codePostal.value.slice(0, -1);
         msgCP.innerHTML = "il ne faut ecrire que des chiffres !!";
         codePostal.style.border = "3px solid red";
+        croix.style.display="flex";
         flag3 = false;
     } else if (valeur.length > 5) {
         codePostal.value = codePostal.value.slice(0, -1);
         msgCP.innerHTML = "il ne faut pas depasser 5 chiffre !!";
+        croix.style.display="flex";
         // codePostal.style.border = "3px solid green";
         flag3 = false;
     } else if (codePostal.checkValidity()) {
         codePostal.style.border = "3px solid green";
+        croix.style.display="none";
+        valide.style.display="flex";
         msgCP.innerHTML = "";
         flag3 = true;
     }
@@ -151,5 +171,7 @@ function btnEnvoyer(flag1, flag2, flag3) {
 // var esp =(codePostal.parentNode).parentNode.getElementsByClassName("espace")[0];
 // esp.style.border=("5px solid green")
 
-var croixS = codePostal.parentNode.getElementsByClassName("croix")[0];
-croixS.style.display = "none";
+
+var valide = codePostal.parentNode.getElementsByClassName("valide")[0];
+var croix = codePostal.parentNode.getElementsByClassName("croix")[0];
+// croixS.style.display = "none";
