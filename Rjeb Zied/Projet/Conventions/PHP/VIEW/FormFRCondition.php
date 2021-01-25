e8c8286dafb370c2757f7ce58f9c247c49143585
RZied <galaxyzied@gmail.com>
2021-01-25 17:15:46 +0100

    25/01/2021 17:15:45,14

--- Rjeb Zied/Projet/Conventions/PHP/VIEW/FormFRCondition.php
+++ Rjeb Zied/Projet/Conventions/PHP/VIEW/FormFRCondition.php
@@ -15,17 +15,17 @@
             </div>
             <div class="ligne">
                 <div class="border double">Début de journée</div>
-                <div class="border"></div>
-                <div class="border"></div>
-                <div class="border"></div>
+                <div class="border"><input type="text"></div>
+                <div class="border"><input type="text"></div>
+                <div class="border"><input type="text"></div>
                 <div class="border"></div>
                 <div class="border"></div>
                 <div class="border"></div>
             </div>
             <div class="ligne">
                 <div class="border double">Début déjeuner</div>
-                <div class="border"></div>
-                <div class="border"></div>
+                <div class="border"><input type="text"></div>
+                <div class="border"><input type="text"></div>
                 <div class="border"></div>
                 <div class="border"></div>
                 <div class="border"></div>
@@ -99,7 +99,7 @@
                     </div>
                 </div>
             </div>
-                    <div class="espaceHor"></div>
+            <div class="espaceHor"></div>
 
             <div class="info">
                 <div class="label centre demi">Déplacements occasionnés par le stage</div>
@@ -112,7 +112,7 @@
                     <label for="non">NON</label>
                 </div>
             </div>
-                    <div class="espaceHor"></div>
+            <div class="espaceHor"></div>
             <div class="info">
                 <div class="label centre demi">Fréquence</div>
                 <div class="colonne">
@@ -130,9 +130,195 @@
                     </div>
                 </div>
             </div>
-                    <div class="espaceHor"></div>
+            <div class="espaceHor"></div>
+            <div class="info">
+                <div class="label centre demi">Modes de déplacement</div>
+                <div class="colonne">
+                    <div class="info">
+                        <input type="checkbox" id="" name="vehEntreprise">
+                        <label for="vehEntreprise">Véhicule de l'entreprise </label>
+                        <div class="mini"></div>
+                        <input type="checkbox" id="" name="vehStagiaire">
+                        <label for="vehStagiaire">Véhicule personnel du stagiaire </label>
+                    </div>
+                    <div class="info centre">
+                        <input type="checkbox" id="" name="Autre">
+                        <label for="Autre">Autre (préciser) : </label>
+                        <input type="text">
+                    </div>
+                </div>
+            </div>
+            <div class="espaceHor"></div>
 
         </fieldset>
-<!-- •	demandent une attestation de formation règlementaire: dans la table stage -->
+        <div class="espaceHor"></div>
+        <div class="espaceHor"></div>
+        <div class="espaceHor"></div>
+        <fieldset>
+            <legend>Les activités
+            </legend>
+            <div class="info">
+                <div class="label centre demi">demandent une attestation de formation règlementaire</div>
+                <div class="colonne">
+                    <div class="info">
+                        <input type="checkbox" id="" name="lieu">
+                        <label for="lieu">OUI</label>
+                        <div class="mini"></div>
+                        <input type="checkbox" id="" name="Chantier">
+                        <label for="Chantier">NON</label>
+                        <div class="mini"></div>
+                    </div>
+                    <div class="info centre">
+                        <input type="checkbox" id="" name="Autre">
+                        <label for="Autre">Si oui, laquelle : </label>
+                        <input type="text">
+                    </div>
+                </div>
+            </div>
+            <div class="espaceHor"></div>
+            <div class="info">
+                <div class="label centre demi">exige une visite médicale d'aptitude:</div>
+                <div class="colonne">
+                    <div class="info">
+                        <input type="checkbox" id="" name="lieu">
+                        <label for="lieu">OUI</label>
+                        <div class="mini"></div>
+                        <input type="checkbox" id="" name="Chantier">
+                        <label for="Chantier">NON</label>
+                        <div class="mini"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="espaceHor"></div>
+            <div class="info">
+                <div class="label centre demi">comportent des travaux dangereux </div>
+                <div class="colonne">
+                    <div class="info">
+                        <input type="checkbox" id="" name="lieu">
+                        <label for="lieu">OUI</label>
+                        <div class="mini"></div>
+                        <input type="checkbox" id="" name="Chantier">
+                        <label for="Chantier">NON</label>
+                        <div class="mini"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="espaceHor"></div>
+            <div class="info">
+                <div class="label centre demi">Si oui, préciser</div>
+                <div class="colonne">
+                    <div class="info colonne">
+                        <div class="info">
+                            <div>
+                                <input type="checkbox" id="" name="lieu">
+                                <label for="lieu">Agents chimiques dangereux </label>
+                            </div>
+                            <div class="mini"></div>
+                            <div>
+                                <input type="checkbox" id="" name="Chantier">
+                                <label for="Chantier"> Milieu confiné </label>
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="info">
+                            <div>
+                                <input type="checkbox" id="" name="lieu">
+                                <label for="lieu">Agents biologiques </label>
+                            </div>
+                            <div class="mini"></div>
+                            <div>
+                                <input type="checkbox" id="" name="Chantier">
+                                <label for="Chantier">Travaux en contact avec du verre ou du métal en fusion</label>
+
+
+                            </div>
+
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="" name="lieu">
+                            <label for="lieu">Vibrations mécaniques</label>
+
+                            <!-- <div class="mini"></div> -->
+                            <div>
+                                <input type="checkbox" id="" name="Chantier">
+                                <label for="Chantier">Manutentions manuelles</label>
+                            </div>
+
+                        </div>
+                        <div>
+                            <input type="checkbox" id="" name="lieu">
+                            <label for="lieu">Rayonnements </label>
+
+                            <!-- <div class="mini"></div> -->
+                            <div>
+                                <input type="checkbox" id="" name="Chantier">
+                                <label for="Chantier">Risques électriques </label>
+                            </div>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="" name="lieu">
+                            <label for="lieu">Milieu hyperbare </label>
+
+                            <!-- <div class="mini"></div> -->
+                            <div>
+                                <input type="checkbox" id="" name="Chantier">
+                                <label for="Chantier">Utilisation de machines </label>
+                            </div>
+
+                        </div>
+                        <div>
+                            <input type="checkbox" id="" name="lieu">
+                            <label for="lieu">Températures extrêmes</label>
+
+                            <!-- <div class="mini"></div> -->
+                            <div>
+                                <input type="checkbox" id="" name="Chantier">
+                                <label for="Chantier">Travaux en hauteur </label>
+                            </div>
+
+                        </div>
+                        <div>
+                            <input type="checkbox" id="" name="lieu">
+                            <label for="lieu">Effondrement et ensevelissement </label>
+
+                            <!-- <div class="mini"></div> -->
+                            <div>
+                                <input type="checkbox" id="" name="Chantier">
+                                <label for="Chantier">Contact avec des animaux</label>
+                            </div>
+
+                        </div>
+                        <div>
+                            <input type="checkbox" id="" name="lieu">
+                            <label for="lieu">Appareils sous pression</label>
+
+                            <!-- <div class="mini"></div> -->
+                            <div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+        </fieldset>
+        <div class="espaceHor"></div>
+        <div class="espaceHor"></div>
     </form>
 </section>
+
+
+<?php
+function jours($numJours,$momentJours)
+{
+    echo'<div class="border"><input type="text" id=".'$momentJours'."></div>';
+
+}
+
+?>
